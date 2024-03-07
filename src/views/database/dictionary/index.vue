5ee36a6d3d91afc36c573413f47959d236a32437
Kagurazaka Chisa <sofiacabello666@outlook.com>
2024-03-07 12:49:15 +0800

    优化了选项管理界面

--- src/views/database/dictionary/index.vue
+++ src/views/database/dictionary/index.vue
@@ -2,122 +2,137 @@
   <div class="dictionary-container">
     <div class="drug-producer">
       <h1 class="title">试剂制造商</h1>
-      <el-tag
-        :key="tag"
-        v-for="tag in tagList.producerTags"
-        closable
-        :disable-transitions="false"
-        @close="openDialog(tagList.producerTags,tag,'producerTags')"
-      >
-        {{ tag }}
-      </el-tag>
-      <el-input
-        class="input-new-tag"
-        v-if="visibleList.producerTags"
-        v-model="inputList.producerTags"
-        ref="saveTagInput"
-        size="small"
-        @blur="handleInputConfirm('producerTags')"
-        @keyup.escape.native="visibleList.producerTags = false"
-      >
-      </el-input>
-      <el-button v-else class="button-new-tag" size="small" @click="showInput('producerTags')">+ New Tag</el-button>
+      <div class="tag-container">
+        <el-tag
+          class="tag-item"
+          :key="tag"
+          v-for="tag in tagList.producerTags"
+          closable
+          :disable-transitions="false"
+          @close="openDialog(tagList.producerTags,tag,'producerTags')"
+        >
+          {{ tag }}
+        </el-tag>
+        <el-input
+          class="input-new-tag"
+          v-if="visibleList.producerTags"
+          v-model="inputList.producerTags"
+          ref="saveTagInput"
+          size="small"
+          @blur="handleInputConfirm('producerTags')"
+          @keyup.escape.native="visibleList.producerTags = false"
+        >
+        </el-input>
+        <el-button v-else class="button-new-tag" size="small" @click="showInput('producerTags')">+ New Tag</el-button>
+    </div>
     </div>
 
     <div class="lab-location">
       <h1 class="title">实验室位置</h1>
-      <el-tag
-        :key="tag"
-        v-for="tag in tagList.labTags"
-        closable
-        :disable-transitions="false"
-        @close="openDialog(tagList.labTags, tag, 'labTags')"
-      >
-        {{ tag }}
-      </el-tag>
-      <el-input
-        class="input-new-tag"
-        v-if="visibleList.labTags"
-        v-model="inputList.labTags"
-        ref="saveTagInput"
-        size="small"
-        @blur="handleInputConfirm('labTags')"
-        @keyup.escape.native="visibleList.labTags = false"
-      >
-      </el-input>
-      <el-button v-else class="button-new-tag" size="small" @click="showInput('labTags')">+ New Tag</el-button>
+      <div class="tag-container">
+        <el-tag
+          class="tag-item"
+          :key="tag"
+          v-for="tag in tagList.labTags"
+          closable
+          :disable-transitions="false"
+          @close="openDialog(tagList.labTags, tag, 'labTags')"
+        >
+          {{ tag }}
+        </el-tag>
+        <el-input
+          class="input-new-tag"
+          v-if="visibleList.labTags"
+          v-model="inputList.labTags"
+          ref="saveTagInput"
+          size="small"
+          @blur="handleInputConfirm('labTags')"
+          @keyup.escape.native="visibleList.labTags = false"
+        >
+        </el-input>
+        <el-button v-else class="button-new-tag" size="small" @click="showInput('labTags')">+ New Tag</el-button>
+    </div>
     </div>
 
     <div class="drug-location">
       <h1 class="title">试剂存储位置</h1>
-      <el-tag
-        :key="tag"
-        v-for="tag in tagList.locationTags"
-        closable
-        :disable-transitions="false"
-        @close="openDialog(tagList.locationTags, tag, 'locationTags')"
-      >
-        {{ tag }}
-      </el-tag>
-      <el-input
-        class="input-new-tag"
-        v-if="visibleList.locationTags"
-        v-model="inputList.locationTags"
-        ref="saveTagInput"
-        size="small"
-        @blur="handleInputConfirm('locationTags')"
-        @keyup.escape.native="visibleList.locationTags = false"
-      >
-      </el-input>
-      <el-button v-else class="button-new-tag" size="small" @click="showInput('locationTags')">+ New Tag</el-button>
+      <div class="tag-container">
+        <el-tag
+          class="tag-item"
+          :key="tag"
+          v-for="tag in tagList.locationTags"
+          closable
+          :disable-transitions="false"
+          @close="openDialog(tagList.locationTags, tag, 'locationTags')"
+        >
+          {{ tag }}
+        </el-tag>
+        <el-input
+          class="input-new-tag"
+          v-if="visibleList.locationTags"
+          v-model="inputList.locationTags"
+          ref="saveTagInput"
+          size="small"
+          @blur="handleInputConfirm('locationTags')"
+          @keyup.escape.native="visibleList.locationTags = false"
+        >
+        </el-input>
+        <el-button v-else class="button-new-tag" size="small" @click="showInput('locationTags')">+ New Tag</el-button>
+      </div>
     </div>
 
     <div class="purchase-source">
       <h1 class="title">购买渠道</h1>
-      <el-tag
-        :key="tag"
-        v-for="tag in tagList.sourceTags"
-        closable
-        :disable-transitions="false"
-        @close="openDialog(tagList.sourceTags, tag, 'sourceTags')"
-      >
-        {{ tag }}
-      </el-tag>
-      <el-input
-        class="input-new-tag"
-        v-if="visibleList.sourceTags"
-        v-model="inputList.sourceTags"
-        ref="saveTagInput"
-        size="small"
-        @blur="handleInputConfirm('sourceTags')"
-        @keyup.escape.native="visibleList.sourceTags = false"
-      >
-      </el-input>
-      <el-button v-else class="button-new-tag" size="small" @click="showInput('sourceTags')">+ New Tag</el-button>
+      <div class="tag-container">
+        <el-tag
+          class="tag-item"
+          :key="tag"
+          v-for="tag in tagList.sourceTags"
+          closable
+          :disable-transitions="false"
+          @close="openDialog(tagList.sourceTags, tag, 'sourceTags')"
+        >
+          {{ tag }}
+        </el-tag>
+        <el-input
+          class="input-new-tag"
+          v-if="visibleList.sourceTags"
+          v-model="inputList.sourceTags"
+          ref="saveTagInput"
+          size="small"
+          @blur="handleInputConfirm('sourceTags')"
+          @keyup.escape.native="visibleList.sourceTags = false"
+        >
+        </el-input>
+        <el-button v-else class="button-new-tag" size="small" @click="showInput('sourceTags')">+ New Tag</el-button>
+      </div>
     </div>
 
     <div class="waste-type">
       <h1 class="title">废弃类型</h1>
-      <el-tag
-        :key="tag"
-        v-for="tag in tagList.wasteTags"
-        closable
-        :disable-transitions="false"
-        @close="openDialog(tagList.wasteTags, tag, 'wasteTags')"
-      >
-        {{ tag }}
-      </el-tag>
-      <el-input
-        class="input-new-tag"
-        v-if="visibleList.wasteTags"
-        v-model="inputList.wasteTags"
-        ref="saveTagInput"
-        size="small"
-        @blur="handleInputConfirm('wasteTags')"
-        @keyup.escape.native="visibleList.wasteTags = false"
-      >
-      </el-input>
-      <el-button v-else class="button-new-tag" size="small" @click="showInput('wasteTags')">+ New Tag</el-button>
+      <div class="tag-container">
+        <el-tag
+          class="tag-item"
+          :key="tag"
+          v-for="tag in tagList.wasteTags"
+          closable
+          :disable-transitions="false"
+          @close="openDialog(tagList.wasteTags, tag, 'wasteTags')"
+        >
+          {{ tag }}
+        </el-tag>
+        <el-input
+          class="input-new-tag"
+          v-if="visibleList.wasteTags"
+          v-model="inputList.wasteTags"
+          ref="saveTagInput"
+          size="small"
+          @blur="handleInputConfirm('wasteTags')"
+          @keyup.escape.native="visibleList.wasteTags = false"
+        >
+        </el-input>
+        <el-button v-else class="button-new-tag" size="small" @click="showInput('wasteTags')">+ New Tag</el-button>
+      </div>
     </div>
 
     <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" center>
@@ -279,35 +294,33 @@ export default {
   font-size: 15px;
 }
 
-.button-new-tag {
-  margin-left: 10px;
-  height: fit-content;
-}
-
 .dictionary-container {
   padding: 10px;
   // 设定内部元素的间距
   .drug-producer,
   .lab-location,
   .drug-location,
+  .waste-type,
   .purchase-source {
     margin-bottom: 20px;
+    .tag-container {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: flex-start;
+      .tag-item {
+        margin-right: 10px;
+        margin-bottom: 10px;
+      }
+      .input-new-tag {
+        width: 90px;
+        margin-bottom: 10px;
+      }
+      .button-new-tag {
+        margin-bottom: 10px;
+      }
+    }
   }
 }
 
-.el-tag + .el-tag {
-  margin-left: 10px;
-}
-.button-new-tag {
-  margin-left: 10px;
-  height: 32px;
-  line-height: 30px;
-  padding-top: 0;
-  padding-bottom: 0;
-}
-.input-new-tag {
-  width: 90px;
-  margin-left: 10px;
-  vertical-align: bottom;
-}
+
 </style>
